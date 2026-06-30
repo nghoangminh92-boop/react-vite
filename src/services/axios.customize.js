2ff688bb15e0922bbdba79d005fa985d9e6b2110
Ren <nghoangminh92@oangminh92gmail.inh92gmailcomgit config --global user.name Rengit commit -m first>
2026-06-30 19:23:37 +0900

    tiếp

--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true
 });
 
 
